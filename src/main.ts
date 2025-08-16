c247befcb4a7b6be19f2ca7a1027c1bee65cf4e5
Fullstackerpr <bahodirnabijanov782@gmail.com>
2025-08-16 18:40:45 +0500

    pipes

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const PORT = Number(process.env.PORT) || 3000;
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    }),
+  );
+
   const config = new DocumentBuilder()
     .setTitle('My API')
     .setDescription('API documentation')
     .setVersion('1.0')
-    .addBearerAuth() 
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/', app, document);
 
-  await app.listen(PORT, () =>
-    console.log('Server running on port:', PORT),
-  );
+  await app.listen(PORT, () => console.log('Server running on port:', PORT));
 }
 bootstrap();
